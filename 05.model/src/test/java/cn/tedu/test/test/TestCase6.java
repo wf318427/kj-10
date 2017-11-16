22d9a7fd2475e3c71dab8dd3e27e42f069528b03
zhangfei <1530181651@qq.com>
2017-11-16 22:49:02 +0800

    首次提交

--- 05.model/src/test/java/cn/tedu/test/test/TestCase6.java
+++ 05.model/src/test/java/cn/tedu/test/test/TestCase6.java
@@ -1,18 +1,33 @@
 package cn.tedu.test.test;
 
+import cn.tedu.entity.Emp;
 import org.junit.Test;
 import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.util.SerializationUtils;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Created by Administrator on 17.11.12.
  */
-public class TestCase6 extends TestBase {
+public class TestCase6  {
     @Test
     public void test(){
         System.out.println(System.getenv());
     }
     @Test
     public void test1(){
+        Emp p=new Emp();
+        p.setName("caocao");
+        p.setAge(45);
+        p.setId(1);
+        byte[] b= SerializationUtils.serialize(p);
+        System.out.println(new String(b));
+        System.out.println(SerializationUtils.deserialize(b));
+
 
     }
 }
